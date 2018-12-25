3599eeaf1cc0718e02cbe0fa105efc36977753dc
kaustubh <kaustubhtapke@yahoo.in>
2018-12-26 02:04:32 +0530

    rvm bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,8 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-#set :rvm_map_bins, %w{gem rake ruby rails bundle}
-set :default_shell, "/bin/bash -l"
+set :rvm1_map_bins, %w{gem rake ruby rails bundle}
+#set :default_shell, "/bin/bash -l"
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
