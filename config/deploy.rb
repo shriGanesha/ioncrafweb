a8516be5c36359389b8c4a3d586f8ba0ba67834d
kaustubh <kaustubhtapke@yahoo.in>
2018-12-25 17:26:53 +0530

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
+set :rvm_type, :system
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
