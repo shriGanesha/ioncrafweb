7e4ee3a8367dcaa41f5571a1ca3f0c8406b4290d
kaustubh <kaustubhtapke@yahoo.in>
2018-12-27 17:32:50 +0530

    two folders

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 server '13.233.206.7', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:kaustubhtapke/ioncrafweb.git'
-set :application,     'ioncrafweb'
+set :application,     'ioncrafweb_ptoduction'
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
